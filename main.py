0021acb0e38d22a4e88d411c6c413c0349b1a865
Uladzimir Kandraciuk <146672155+Wl0cKk@users.noreply.github.com>
2025-09-28 14:41:46 +0300

    initial design

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.clock import Clock
 
-App().run()
+class ShddApp(App):
+    status_text = StringProperty("Paste YouTube URL")
+    
+    def download_short(self):
+        url = self.root.ids.url_input.text
+        if not url.strip():
+            self.status_text = "Please enter a URL first!"
+            return
+            
+        self.status_text = "Downloading..."
+        Clock.schedule_once(self.finish_download, 2)
+    
+    def finish_download(self, dt):
+        self.status_text = "Download completed!"
+        self.root.ids.url_input.text = ""
+
+if __name__ == '__main__':
+    ShddApp().run()
